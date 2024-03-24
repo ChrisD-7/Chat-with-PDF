5d954a1624e441aa06c76b908078dd420128ab67
ChrisD-7 <157317426+ChrisD-7@users.noreply.github.com>
2024-03-23 21:41:37 -0400

    first run

--- app.py
+++ app.py
@@ -2,100 +2,105 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
-
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS                                # for vector embeddings 
-from langchain_google_genai import GoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain           # for question chats and prompts
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-
-
-
 load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-genai.configure(api_key=os.getenv("GENAI_API_KEY"))
 
 
-#REad PDF go through pages and extract text.
+
+
+
 def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf_doc in pdf_docs:
-
-        pdf = PdfReader(pdf)
-
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
         for page in pdf_reader.pages:
-            text += page.extract_text()
-    
-    return text
+            text+= page.extract_text()
+    return  text
+
+
 
 def get_text_chunks(text):
-    splitter = RecursiveTextSplitter(chunk_size=10000, chunk_overlap=1000) #here we are splitting the text into chunks of 10000 characters with an overlap of 1000 characters
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
     return chunks
 
+
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embeddings-001")
-    vector_store = FAISS.from_text(text_chunks, embeddings=embeddings)
-    vector_store.save_local("fiass_index")
-    return vector_store
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
 
 
 def get_conversational_chain():
-    prompt_template = '''
-    Answer the question as detailed as possible from the provided context, make sure to provide the details and the reasoning behind your answer, if the answer 
-    is not present in the context just say "Answer not available in the context", do not provide wrong answers.
-    Context: \n {context}? \n
-    Question: \n {question}
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
 
     Answer:
+    """
 
-    '''
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
 
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-
-    chain = load_qa_chain(model, chain_type='stuff',prompt=prompt)
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
     return chain
 
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(mode1="models/embedding-0Ø1")
 
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
     new_db = FAISS.load_local("faiss_index", embeddings)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
 
+    
     response = chain(
-        {"input_documents": docs, "question": user_question},
-        return_only_outputs=True)
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
 
     print(response)
+    st.write("Reply: ", response["output_text"])
 
-    st.write("Reply:", response["output_text"])
+
 
 
 def main():
-    st.set_page_config('Chat with multiple PDF')
-    st.header("Chat with multiple PDF using GEMINI")
+    st.set_page_config("Chat with PDF")
+    st.header("Chat with PDF using GEMINI")
 
-    user_question = st.text_input("Ask a question based on the PDF:")
+    user_question = st.text_input("Ask a Question the PDF Files")
 
     if user_question:
         user_input(user_question)
-    
+
     with st.sidebar:
-        st.title("Menu")
-        pdf_docs = st.file_uploader("Upload PDF")
-        if st.button("Submit and Process PDF"):
-            with st.spinner("Processing PDF..."):
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_pdf_text(raw_text)
+                text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks)
-                st.success("PDF Processed Successfully")
+                st.success("Done")
+
+
 
 if __name__ == "__main__":
     main()
